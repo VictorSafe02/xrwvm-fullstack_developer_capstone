df94b9c6c6d991ec18cedecba68fe02ee70c2ba0
Victor Arowosafe <victor.arowosafe@yahoo.com>
2024-08-14 23:43:33 +0100

    third day part two

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
 
     path(route='logout', view=views.logout_user, name='logout'),
 
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
