d47dc265bd3db0baec09de7a9d7ae57f6c7c5cb3
Victor Arowosafe <victor.arowosafe@yahoo.com>
2024-08-18 21:12:45 +0100

    Module 5 test

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -32,8 +32,8 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake,
-                                # Many-to-One relationship
-                                on_delete=models.CASCADE)
+                                  # Many-to-One relationship
+                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -43,10 +43,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-                              validators=[
+                               validators=[
                                             MaxValueValidator(2023),
                                             MinValueValidator(2015)
-                              ])
+                               ])
     # Other fields as needed
 
     def __str__(self):
